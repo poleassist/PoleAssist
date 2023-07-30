3da5a9f13bf189216666ac4370df7f7efc4117b6
Amanda <windsoramanda@outlook.com>
2023-07-30 16:09:43 +1000

    Update index and delete unused files

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    String[] level1Tricks = new String[20];
-        level1Tricks[0] = "Lazy Drag";
-        System.out.print(level1Tricks[0]);
-        
-    String[] level2Tricks = new String[20];
-    level2Tricks[0] = "Step Up Spin";
-    System.out.print(level2Tricks[0]);
-}
