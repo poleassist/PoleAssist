a1c6662ff2e4adc1abf777eabcd5179093dae713
Amanda <windsoramanda@outlook.com>
2023-08-15 09:46:39 +1000

    Update skills.php

--- skills.php
+++ skills.php
@@ -57,24 +57,8 @@
       </div>
     </nav>
 
-<?php
-$arr_result = array(
-    array(
-        'title' => 'Accordion Item #1',
-        'content' => '<strong>This is the first item\'s accordion body.</strong>'
-    ),
-    array(
-        'title' => 'Accordion Item #2',
-        'content' => '<strong>This is the second item\'s accordion body.</strong>'
-    ),
-    array(
-        'title' => 'Accordion Item #3',
-        'content' => '<strong>This is the third item\'s accordion body.</strong>'
-    ),
-);
-?>
 
-<div class="accordion" id="accordionExample">
+<!-- <div class="accordion" id="accordionExample">
     <?php for($i=0; $i<count($arr_result); $i++)  ?>
         <div class="accordion-item">
             <h2 class="accordion-header" id="heading<?php echo $i; ?>">
@@ -88,7 +72,7 @@ $arr_result = array(
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
